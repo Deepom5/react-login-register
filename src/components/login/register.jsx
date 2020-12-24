de6175eaca05a1f43ca2058c7cdccef0b182da9e
Deepom5 <chowdhurydeepom@gmail.com>
2020-12-25 03:07:07 +0530

    Initial commit

---
+++ src/components/login/register.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import loginImg from "../../login.svg";
+
+export class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      role: 'user'
+    };
+  }
+
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="header">Register</div>
+        <div className="content">
+          <div className="image">
+            <img src={loginImg} />
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" placeholder="username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="email" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="text" name="password" placeholder="password" />
+            </div>
+            <div className="form-group">
+            <select value={this.state.role}>
+            <option value="admin">Admin</option>
+            <option value="user">User</option>
+              </select>
+              </div>
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Register
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
